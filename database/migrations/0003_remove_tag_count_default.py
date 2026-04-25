b8d7666d9ffac3eead5bda8fd9819cdbfbf4fbb2
Peter Ruse <ruse.peter@gmail.com>
2026-04-25 18:27:42 -0400

    add rollback query

--- database/migrations/0003_remove_tag_count_default.py
+++ database/migrations/0003_remove_tag_count_default.py
@@ -3,5 +3,8 @@ from yoyo import step
 step(
     """
     ALTER TABLE events ALTER COLUMN tags_count DROP DEFAULT;
+    """,
     """
+    ALTER TABLE events ALTER COLUMN tags_count SET DEFAULT 0;
+    """,
 )
